0d96a2c526e413eb05ac2de274bed91f21c45ead
xutongbao <1183391880@qq.com>
2019-07-24 11:43:44 +0800

    登录

--- test/src/pages/login/Login.js
+++ test/src/pages/login/Login.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import {Input, Button, Select} from 'antd'
-import intl from 'react-intl-universal'
-import {jsEncrypt} from '../../utils/index.js'
-import actions from '../../store/task/actionTypes'
-import Api from '../../api';
+import {Input, Button} from 'antd'
+import Api from '../../api'
+import {jsEncrypt} from '../../utils'
 import * as keyCode from '../../api/keyCode.js'
-import { from } from 'rxjs';
+import './index.css'
 
-const {Option} = Select
+let usernameDom, captchaDom
 class Login extends React.Component {
   constructor(props) {
     super(props)
@@ -17,7 +14,6 @@ class Login extends React.Component {
       password: '',
       captcha: '',
       captchaSvg: '',
-      language: 'zh-CN',
     }
   }
   render() {
@@ -26,60 +22,49 @@ class Login extends React.Component {
       password,
       captcha,
       captchaSvg,
-      language,
     } = this.state
     return (
-      <div className="m-login">
-        <div>
-          <div>
-            <Select value={language} onChange={this.handleSelect.bind(this)}>
-              <Option value="zh-CN">中文</Option>
-              <Option value="en-US">English</Option>
-            </Select>
-          </div>
-          <div>{intl.get('login.loginTitle')}</div>
-          <div className="m-login-row">
-            <Input 
-              placeholder={intl.get('login.usernamePlaceholder')} 
-              value={username} 
-              ref={(input) => this.userNameInput = input}
-              onChange={this.handleInput.bind(this, 'username')}></Input>
-          </div>
-          <div>
-            <Input 
-              type="password" 
-              placeholder="请输入密码" 
-              value={password} 
-              autoComplete="off"
-              onChange={this.handleInput.bind(this, 'password')}></Input>
-          </div>   
-          <div>
-            <Input 
-              type="text" 
-              placeholder="请输入验证码" 
-              value={captcha} 
-              autoComplete="off"
-              ref={(input) => this.captchaInput = input}
-              onKeyDown={this.handleEnter.bind(this)}               
-              onChange={this.handleInput.bind(this, 'captcha')}></Input>
-              <span dangerouslySetInnerHTML={{__html: captchaSvg}} onClick={this.getCaptcha.bind(this)}>
-              </span>
-          </div>                   
-          <div>
-            <Button onClick={this.handleLogin.bind(this)}>{intl.get('login.login')}</Button>
-          </div>
-        </div>        
+      <div className="m-login">  
+        <div className="m-login-title">
+          后台管理系统
+        </div>   
+        <div className="m-login-row">
+          <Input 
+            type="text" 
+            placeholder="请输入用户名"
+            value={username} 
+            ref={(input) => usernameDom = input}
+            onChange={this.handleIput.bind(this, 'username')} ></Input>
+        </div>
+        <div className="m-login-row">
+          <Input 
+            type="password" 
+            placeholder="请输入密码"
+            value={password} 
+            onChange={this.handleIput.bind(this, 'password')} ></Input>
+        </div>    
+        <div className="m-login-row">
+          <Input 
+            type="text" 
+            placeholder="请输入验证码"
+            value={captcha} 
+            onKeyDown={this.handleEnter.bind(this)}
+            ref={(input) => captchaDom = input}
+            onChange={this.handleIput.bind(this, 'captcha')} ></Input>
+          <span dangerouslySetInnerHTML={{__html: captchaSvg}} onClick={this.getCaptcha.bind(this)}></span>
+        </div>   
+        <div className="m-login-row">
+          <Button onClick={this.handleLogin.bind(this)}>登录</Button>
+        </div>           
 			</div>
     );
   }
 }
 
-//生命周期
 Object.assign(Login.prototype, {
   componentDidMount() {
-    this.userNameInput.focus()
+    usernameDom.focus()
     this.getCaptcha()
-    this.languageInit()
   }
 })
 
@@ -97,79 +82,39 @@ Object.assign(Login.prototype, {
       captcha,
     }
     Api.login(data).then((res) => {
-      console.log(res)
+      if (res.code === keyCode.SUCCESS) {
+        alert('登录成功！')
+      }
     }).catch(() => {
       this.getCaptcha()
     })
-
-    console.log(username, password, captcha)
+    
   },
   getCaptcha() {
     Api.captcha().then((res) => {
       if (res.code === keyCode.SUCCESS) {
-        localStorage.setItem('token', res.data.captchaId)
         this.setState({
           captchaSvg: res.data.captcha
         })
-      }
-      console.log(res)
-    })
-  },
-  languageInit() {
-    let currentLocale = localStorage.getItem('language') || 'zh-CN'
-    intl.init({
-      currentLocale: currentLocale,
-      locales: {
-        [currentLocale]: require(`../../i18n/${currentLocale}`).default
+        localStorage.setItem('token', res.data.captchaId)
       }
     })
-    if (!localStorage.getItem('language')) {
-      localStorage.setItem('language', 'zh-CN')
-      this.setState({
-        language: 'zh-CN'
-      })
-    } else {
-      this.setState({
-        language: currentLocale
-      })
-    }
   },
   handleEnter(e) {
-		if (e.keyCode === 13) {
-			this.handleLogin()
-			this.captchaInput.blur()
-		}    
-  }  
+    if (e.keyCode === 13) {
+      this.handleLogin()
+      captchaDom.blur()
+    }
+  }
 })
 
 //受控组件
 Object.assign(Login.prototype, {
-  handleInput(field, e) {
+  handleIput(field, e) {
     this.setState({
       [field]: e.target.value
     })
-  },
-  handleSelect(value) {
-    this.setState({
-      language: value
-    })
-    localStorage.setItem('language', value)
-    window.location.reload()
   }
 })
 
-const mapStateToProps = (state) => {
-  return {
-    
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    TASK_CHANGAE_STATE: (key, value) => {
-      dispatch({ type: actions.TASK_CHANGAE_STATE, key, value });
-    }    
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+export default Login
